e67c791283d24bfc47a82629b2cde7f091a20586
Bikash Sharma <36207673+bikashxsharma@users.noreply.github.com>
2019-10-16 12:47:08 +0300

    lerning_functions_more

--- function_1.js
+++ function_1.js
@@ -15,3 +15,17 @@ let addNumber = function(num1, num2, num3){
 let result = addNumber(5, 6, 7); 
 
 console.log(result);
+
+let addNumber1 = (nuum1, nuum2)=> nuum1+nuum2;
+
+console.log(addNumber1(4,5))
+
+
+let myMulitiplier = function(mul1, mul2){
+        return mul1*mul2;
+
+}
+console.log(myMulitiplier(9,5))
+
+let myMulitiplier1 = (mull1, mull2)=> mull1*mull2;
+console.log(myMulitiplier1(3,23));
