3c994354aadaeb96a641fdffd0494c8ba44e25ec
Bikash Sharma <36207673+bikashxsharma@users.noreply.github.com>
2019-10-16 13:42:10 +0300

    more-functions

---
+++ compare_num.js
@@ -0,0 +1,14 @@
+let result=null;
+let compareNum = function(num1, num2){
+    if (num1<num2)
+    {
+        return result=num1; 
+    }
+    else
+    {
+        return result=num2;
+    }
+
+}
+compareNum(10,-35);
+console.log("The minimun number between two is: " +result)
